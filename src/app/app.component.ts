6e0630543bdc45f4502f1dc925f296a84b0fdc87
DrDokutah <Phamnhatquangbsqnvn@gmail.com>
2024-08-27 12:02:29 +0700

    change page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from "./product/product.component";
-import { BannerComponent } from "./banner/banner.component";
-import { BlogComponent } from "./blog/blog.component";
-import { CartComponent } from "./cart/cart.component";
-import { ShopComponent } from "./shop/shop.component";
-import { UserComponent } from "./user/user.component";
-import { ContactComponent } from "./contact/contact.component";
+import { ProductComponent } from './product/product.component';
+import { BannerComponent } from './banner/banner.component';
+import { BlogComponent } from './blog/blog.component';
+import { CartComponent } from './cart/cart.component';
+import { ShopComponent } from './shop/shop.component';
+import { UserComponent } from './user/user.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, ProductComponent, HomeComponent, BannerComponent, BlogComponent, CartComponent, ShopComponent, UserComponent, ContactComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    HeaderComponent,
+    ProductComponent,
+    HomeComponent,
+    BannerComponent,
+    BlogComponent,
+    CartComponent,
+    ShopComponent,
+    UserComponent,
+    ContactComponent,
+    CommonModule,
+    BlogDetailComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'AntamedWeb';
-  currentPage:string = 'home';
-  
+  currentPage: string = 'home';
+
   ngOnInit(): void {
     console.log(this.currentPage);
+  }
+
+  changePage(currPage: any) {
+    // console.log('after\ncurrPage: ' + currPage);
+    // console.log('this.currentPage: ' + this.currentPage);
+    this.currentPage = currPage;
+    // console.log('after\ncurrPage: ' + currPage);
+    // console.log('this.currentPage: ' + this.currentPage);
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  @Input() currPage: any;
+  @Output() onChangePage = new EventEmitter();
 
+  toHome() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'home';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toShop() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'shop';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toProduct() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'product';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toAbout() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'about';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toBlog() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'blog';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toContact() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'contact';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toUser() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'user';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
+  toCart() {
+    // console.clear();
+    // console.log('before:' + this.currPage)
+    this.currPage = 'cart';
+    this.onChangePage.emit(this.currPage);
+    // console.log('after:' + this.currPage)
+  }
 }
